6cb339add3a4aa839f433f081880812837046548
KennethPappertQU <Kenneth.Pappert@quinnipiac.edu>
2026-02-25 20:55:58 -0500

    third step

--- AnimalPicker.java
+++ AnimalPicker.java
@@ -7,42 +7,50 @@ public class AnimalPicker {
         System.out.println("Would you like to see a dog or a cat? ");
         String animal = in.next();
 
+        boolean isChosen = false;
 
-        if (animal.equals("dog")) {
-            System.out.println("    ___\r\n" + //
-                                " __/ / \\\r\n" + //
-                                "|    |/\\\r\n" + //
-                                "|_--\\   \\              /-\r\n" + //
-                                "     \\   \\-___________/ /\r\n" + //
-                                "      \\                :\r\n" + //
-                                "      |                :\r\n" + //
-                                "      |       ___ \\    )\r\n" + //
-                                "       \\|  __/     \\  )\r\n" + //
-                                "        | /         \\  \\\r\n" + //
-                                "        |l           ( l\r\n" + //
-                                "        |l            ll\r\n" + //
-                                "        |l            |l\r\n" + //
-                                "       / l           / l\r\n" + //
-                                "       --/           --\r\n" + //
-                                "");
-        }
-        else if (animal.equals("cat")) {
-            System.out.println("      _          ___\r\n" + //
-                                "    /' '\\       / \" \\\r\n" + //
-                                "   |  ,--+-----4 /   |\r\n" + //
-                                "   ',/   o  o     --.;\r\n" + //
-                                "--._|_   ,--.  _.,-- \\----.\r\n" + //
-                                "------'--`--' '-----,' VJ  |\r\n" + //
-                                "     \\_  ._\\_.   _,-'---._.'\r\n" + //
-                                "       `--...--``  /\r\n" + //
-                                "         /###\\   | |\r\n" + //
-                                "         |.   `.-'-'.\r\n" + //
-                                "        .||  /,     |\r\n" + //
-                                "       do_o00oo_,.ob\r\n" + //
-                                "");
-        } else {
-            System.out.println("Error: That is not one of the answers. ");
-        
+        while (isChosen) {
+            if (animal.equals("dog")) {
+                System.out.println("    ___\r\n" + //
+                                    " __/ / \\\r\n" + //
+                                    "|    |/\\\r\n" + //
+                                    "|_--\\   \\              /-\r\n" + //
+                                    "     \\   \\-___________/ /\r\n" + //
+                                    "      \\                :\r\n" + //
+                                    "      |                :\r\n" + //
+                                    "      |       ___ \\    )\r\n" + //
+                                    "       \\|  __/     \\  )\r\n" + //
+                                    "        | /         \\  \\\r\n" + //
+                                    "        |l           ( l\r\n" + //
+                                    "        |l            ll\r\n" + //
+                                    "        |l            |l\r\n" + //
+                                    "       / l           / l\r\n" + //
+                                    "       --/           --\r\n" + //
+                                    "");
+                isChosen = true;
+            }
+            else if (animal.equals("cat")) {
+                System.out.println("      _          ___\r\n" + //
+                                    "    /' '\\       / \" \\\r\n" + //
+                                    "   |  ,--+-----4 /   |\r\n" + //
+                                    "   ',/   o  o     --.;\r\n" + //
+                                    "--._|_   ,--.  _.,-- \\----.\r\n" + //
+                                    "------'--`--' '-----,' VJ  |\r\n" + //
+                                    "     \\_  ._\\_.   _,-'---._.'\r\n" + //
+                                    "       `--...--``  /\r\n" + //
+                                    "         /###\\   | |\r\n" + //
+                                    "         |.   `.-'-'.\r\n" + //
+                                    "        .||  /,     |\r\n" + //
+                                    "       do_o00oo_,.ob\r\n" + //
+                                    "");
+                isChosen = true;
+
+            } else {
+                System.out.println("Error: That is not one of the answers. ");
+                isChosen = true;
+            
+            }
         }
     }
 }
+
