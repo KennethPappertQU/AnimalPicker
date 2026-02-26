fb50b2f1daa8e5421c178b353ca23560db841c51
KennethPappertQU <Kenneth.Pappert@quinnipiac.edu>
2026-02-25 21:04:47 -0500

    fourth step

--- AnimalPicker.java
+++ AnimalPicker.java
@@ -4,53 +4,39 @@ public class AnimalPicker {
     public void main(String [] args){
         Scanner in = new Scanner(System.in);
 
-        System.out.println("Would you like to see a dog or a cat? ");
+        System.out.println("Would you like to see a dog, a fish, or a cat? ");
         String animal = in.next();
 
         boolean isChosen = false;
 
         while (isChosen) {
             if (animal.equals("dog")) {
-                System.out.println("    ___\r\n" + //
-                                    " __/ / \\\r\n" + //
-                                    "|    |/\\\r\n" + //
-                                    "|_--\\   \\              /-\r\n" + //
-                                    "     \\   \\-___________/ /\r\n" + //
-                                    "      \\                :\r\n" + //
-                                    "      |                :\r\n" + //
-                                    "      |       ___ \\    )\r\n" + //
-                                    "       \\|  __/     \\  )\r\n" + //
-                                    "        | /         \\  \\\r\n" + //
-                                    "        |l           ( l\r\n" + //
-                                    "        |l            ll\r\n" + //
-                                    "        |l            |l\r\n" + //
-                                    "       / l           / l\r\n" + //
-                                    "       --/           --\r\n" + //
-                                    "");
+                System.out.println("/^-----^\\\r\n" + //
+                                        "V  o o  V\r\n" + //
+                                        " |  Y  |\r\n" + //
+                                        "  \\ Q /\r\n" + //
+                                        "  / - \\\r\n" + //
+                                        "  |    \\\r\n" + //
+                                        "  |     \\     )\r\n" + //
+                                        "  || (___\\====\r\n" + //
+                                        "");
                 isChosen = true;
             }
             else if (animal.equals("cat")) {
-                System.out.println("      _          ___\r\n" + //
-                                    "    /' '\\       / \" \\\r\n" + //
-                                    "   |  ,--+-----4 /   |\r\n" + //
-                                    "   ',/   o  o     --.;\r\n" + //
-                                    "--._|_   ,--.  _.,-- \\----.\r\n" + //
-                                    "------'--`--' '-----,' VJ  |\r\n" + //
-                                    "     \\_  ._\\_.   _,-'---._.'\r\n" + //
-                                    "       `--...--``  /\r\n" + //
-                                    "         /###\\   | |\r\n" + //
-                                    "         |.   `.-'-'.\r\n" + //
-                                    "        .||  /,     |\r\n" + //
-                                    "       do_o00oo_,.ob\r\n" + //
-                                    "");
+                System.out.println(" _._     _,-'\"\"`-._\r\n" + //
+                                        "(,-.`._,'(       |\\`-/|\r\n" + //
+                                        "    `-.-' \\ )-`( , o o)\r\n" + //
+                                        "          `-    \\`_`\"'-");
                 isChosen = true;
-
-            } else {
+            }  else {
                 System.out.println("Error: That is not one of the answers. ");
                 isChosen = true;
-            
             }
         }
     }
 }
+
+
+
+
 
